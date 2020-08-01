a15c86f06b9eb257692fc6dad2183ce3131570f4
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-07-31 23:49:09 -0400

    move auth to file

---
+++ src/backend/authentication/index.js
@@ -0,0 +1,52 @@
+const uuid = require('uuid');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+const addAuth = (app) => {
+    passport.use(new LocalStrategy(
+        { usernameField: 'email' },
+        async (email, password, done) => {
+            const dbUser = await User.findBy('email', email);
+            if (!dbUser) return done(null, false, {message: 'no User\n'})
+            const passwordsDoMatch = await bcrypt.compare(password, dbUser.password);
+            if (dbUser.email && passwordsDoMatch) return done(null, dbUser);
+            return done(null, false, { message: 'Invalid credentials.\n' });
+        },
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((userId, done) => {
+        User.find(userId)
+            .then(user => done(null, user))
+            .catch(error => done(error, false));
+    });
+
+    app.set('etag', false);
+    app.use((req, res, next) => {
+        res.set('Cache-Control', 'no-store');
+        next();
+    });
+
+    app.use(session({
+        genid: (req) => {
+            return uuid.v4(); // use UUIDs for session IDs
+        },
+        store: new FileStore(),
+        secret: 'keyboard cat', // should be env var
+        resave: false,
+        saveUninitialized: true,
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
+
+module.exports = addAuth;
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,74 +1,19 @@
 const path = require('path');
 const express = require('express');
-const uuid = require('uuid');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
-const flash = require('connect-flash');
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpack = require("webpack");
 const webpackConfig = require('../../webpack.config');
 
 const compiler = webpack(webpackConfig);
 
-const User = require('./models/user');
 const addAllRoutes = require('./routes');
-const { production } = require('../../knexfile');
+const addAuth = require('./authentication');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-passport.use(new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-        console.log('email: ', email);
-        const dbUser = await User.findBy('email', email);
-        console.log('dbUser: ', dbUser);
-        if (!dbUser) return done(null, false, {message: 'no User\n'})
-        const passwordsDoMatch = await bcrypt.compare(password, dbUser.password);
-        if (dbUser.email && passwordsDoMatch) return done(null, dbUser);
-        return done(null, false, { message: 'Invalid credentials.\n' });
-    },
-));
-
-passport.serializeUser((user, done) => {
-    console.log('Inside serialize user method');
-    done(null, user.id);
-});
-
-passport.deserializeUser((userId, done) => {
-    console.log('in deserialize: ');
-    console.log('userId: ', userId);
-    User.find(userId)
-        .then(user => done(null, user))
-        .catch(error => done(error, false));
-});
-
-app.set('etag', false);
-app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store');
-    next();
-});
-
-app.use(flash());
-
-app.use(session({
-    genid: (req) => {
-        console.log('Inside the session middleware');
-        console.log('middle ware session id:', req.sessionID);
-        return uuid.v4(); // use UUIDs for session IDs
-    },
-    store: new FileStore(),
-    secret: 'keyboard cat', // should be env var
-    resave: false,
-    saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
+addAuth(app);
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(webpackDevMiddleware(compiler, {
