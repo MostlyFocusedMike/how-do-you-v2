872e9fe03b2f35b3a1a40860fcfc2cb11311422b
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-08-29 13:50:41 -0400

    add queries file

--- src/backend/routes/categories/list.js
+++ src/backend/routes/categories/list.js
@@ -2,15 +2,9 @@ const handler = (router) => {
     router.get(
         '/api/v1/categories',
         async (req, res) => {
-            const categories = await req.models.Category.all();
-            const promises = categories.map(category => category.relations('questions'));
-            const questions = await Promise.all(promises);
-            if (req.query.questions) {
-                for (let i = 0; i < categories.length; i++) {
-                    categories[i].questions = questions[i];
-                }
-            }
-            res.send(categories);
+            const { Category } = req.models;
+            const categories = await Category.all();
+            res.json(categories);
         },
     );
 };
